bfbb28137b0ec4bc5d8ab38c250fab804ecb07fe
Ric Musyoki <ric.musyoki@student.moringaschool.com>
2026-08-25 15:01:05 +0300

    Test record defaults

--- apps/records/tests.py
+++ apps/records/tests.py
@@ -1,3 +1,18 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Record
+
+
+class RecordModelTests(TestCase):
+	def test_record_defaults_to_pending(self):
+		user = get_user_model().objects.create_user(username='owner', password='pass12345')
+		record = Record.objects.create(
+			user=user,
+			title='Broken road',
+			description='The road is impassable after rainfall.',
+			type=Record.RecordType.INTERVENTION,
+		)
+
+		self.assertEqual(record.status, Record.Status.PENDING)
+		self.assertEqual(str(record), 'Broken road (Intervention)')
