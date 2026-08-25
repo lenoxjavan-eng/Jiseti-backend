e382355d1a9529b775ca3ec00b0575d84443f0fa
Ric Musyoki <ric.musyoki@student.moringaschool.com>
2026-08-25 14:57:27 +0300

    Register records in admin

--- apps/records/admin.py
+++ apps/records/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Record
+
+
+@admin.register(Record)
+class RecordAdmin(admin.ModelAdmin):
+	list_display = ('id', 'title', 'type', 'status', 'user', 'created_at')
+	list_filter = ('type', 'status', 'created_at')
+	search_fields = ('title', 'description', 'user__username', 'user__email')
+	readonly_fields = ('created_at', 'updated_at')
