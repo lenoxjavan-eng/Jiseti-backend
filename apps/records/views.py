097d74210aa1d2398cc57500d7efda8887979356
Ric Musyoki <ric.musyoki@student.moringaschool.com>
2026-08-25 14:59:54 +0300

    Support pending record updates

--- apps/records/views.py
+++ apps/records/views.py
@@ -5,9 +5,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .serializers import RecordSerializer
-from .services import create_record
+from .services import create_record, update_record
 from .models import Record
-from .permissions import IsOwnerOrAdmin
+from .permissions import IsOwnerOrAdmin, IsPendingRecord
 
 
 class RecordListCreateView(APIView):
@@ -37,4 +37,20 @@ class RecordDetailView(APIView):
 
 	def get(self, request, pk):
 		record = self.get_object(pk)
+		return Response(RecordSerializer(record, context={'request': request}).data)
+
+	def put(self, request, pk):
+		return self._update(request, pk, partial=False)
+
+	def patch(self, request, pk):
+		return self._update(request, pk, partial=True)
+
+	def _update(self, request, pk, *, partial):
+		record = self.get_object(pk)
+		self.check_object_permissions(request, record)
+		if not IsPendingRecord().has_object_permission(request, self, record):
+			return Response({'detail': IsPendingRecord.message}, status=status.HTTP_403_FORBIDDEN)
+		serializer = RecordSerializer(record, data=request.data, partial=partial, context={'request': request})
+		serializer.is_valid(raise_exception=True)
+		record = update_record(record=record, **serializer.validated_data)
 		return Response(RecordSerializer(record, context={'request': request}).data)
