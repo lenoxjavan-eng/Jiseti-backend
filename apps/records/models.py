2ee08de5440f7c7e9b0d1f7981923059fcc98c74
Ric Musyoki <ric.musyoki@student.moringaschool.com>
2026-08-25 14:56:03 +0300

    Define canonical record choices

--- apps/records/models.py
+++ apps/records/models.py
@@ -2,6 +2,16 @@ from django.db import models
 
 
 class Record(models.Model):
+	class RecordType(models.TextChoices):
+		RED_FLAG = 'red-flag', 'Red flag'
+		INTERVENTION = 'intervention', 'Intervention'
+
+	class Status(models.TextChoices):
+		PENDING = 'pending', 'Pending'
+		UNDER_INVESTIGATION = 'under-investigation', 'Under investigation'
+		REJECTED = 'rejected', 'Rejected'
+		RESOLVED = 'resolved', 'Resolved'
+
 	latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
 	longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
 	created_at = models.DateTimeField(auto_now_add=True)
